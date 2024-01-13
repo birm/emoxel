9b96187aaae775fea63bfa1c3a6ac3ed3d54426c
Birm <birm@rbirm.us>
2024-01-13 02:18:09 -0500

    rename and update readme

--- make_dataset.py
+++ make_dataset.py
@@ -3,7 +3,7 @@ import subprocess
 
 def run_emojxel_convert(image_path):
     """Run EmojxelConvert.py for the given image path."""
-    emojxel_command = ["python", "EmojxelConvert.py", image_path]
+    emojxel_command = ["python", "EmoxelConvert.py", image_path]
     subprocess.run(emojxel_command)
 
 def run_html_to_png(html_file_path):
@@ -30,7 +30,7 @@ def main():
         if image_filename.endswith(".jpg") or image_filename.endswith(".png"):
             image_path = os.path.join(original_image_path, image_filename)
 
-            # Run EmojxelConvert.py to generate HTML file
+            # Run EmoxelConvert.py to generate HTML file
             html_output_file = os.path.join(html_output_path, f"{image_filename}.html")
             run_emojxel_convert(image_path)
 
